29873a96046b7b0de2b434aadb8a6e1e5a874329
Esteban Ibañez <e.ibannez.p@gmail.com>
2022-05-18 22:26:47 -0400

    v2

--- src/Etiqueta.js
+++ src/Etiqueta.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { VStack, Container } from "@chakra-ui/react";
+import { VStack, Box, Heading } from "@chakra-ui/react";
 
 const Etiqueta = () => {
   return (
     <VStack bg="teal" h="calc(100vh)">
-      <Container maxW="container.xl" bg="green.400" color="white">
-        <p>Hola Mundo !</p>
-      </Container>
+      <VStack spacing={4} align="stretch" m={10}>
+        <Box
+          maxW="xl"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          rounded="xl"
+          pos="relative"
+          boxShadow="lg"
+          w="100%"
+          p={{ base: 4, lg: 6 }}
+        >
+          <Heading color="white" as="u">
+            H O L A - M U N D O
+          </Heading>
+        </Box>
+      </VStack>
     </VStack>
   );
 };
