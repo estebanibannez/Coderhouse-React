6b802b8710e252d84d04154da19a3851d14d391e
Esteban Ibanez <e.ibannez.p@gmail.com>
2022-06-28 18:50:23 -0400

    style: fixed color chakra ui

--- src/components/Forms/FormContact.jsx
+++ src/components/Forms/FormContact.jsx
@@ -63,7 +63,7 @@ const FormContact = () => {
   };
 
   return (
-    <Container maxW={'3xl'}>
+    <Container maxW={'3xl'} color="white">
       <Stack as={Box} textAlign={'center'} spacing={{ base: 8, md: 14 }} py={{ base: 20, md: 36 }}>
         <Heading
           fontWeight={600}
